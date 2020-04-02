dd9da1e64df48330a5290ee3d4c1ad35dfa326bb
syahaina20 <syahaina20@gmail.com>
2020-04-02 14:04:00 +0800

    Updates

---
+++ precache-manifest.d482b6ec838fa640fd84631a0a294464.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "466ca3eb34c385ebd99d32647c19f8de",
+    "url": "/React-in-github-carousel/index.html"
+  },
+  {
+    "revision": "6781e9532c7811ae8dc4",
+    "url": "/React-in-github-carousel/static/css/main.15d0c7c5.chunk.css"
+  },
+  {
+    "revision": "6da1611f083daed63717",
+    "url": "/React-in-github-carousel/static/js/2.31be3729.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-in-github-carousel/static/js/2.31be3729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6781e9532c7811ae8dc4",
+    "url": "/React-in-github-carousel/static/js/main.45442881.chunk.js"
+  },
+  {
+    "revision": "616116a0c6b670d40e97",
+    "url": "/React-in-github-carousel/static/js/runtime-main.f68095b3.js"
+  },
+  {
+    "revision": "6c2108301ffa8f2adfc839da2a74b8be",
+    "url": "/React-in-github-carousel/static/media/naruto.6c210830.png"
+  },
+  {
+    "revision": "ce07ce4a3ffc4a26079bc04d72afcad7",
+    "url": "/React-in-github-carousel/static/media/silentvoice.ce07ce4a.jpg"
+  },
+  {
+    "revision": "e3a56a10130212d84c5d8bb6c428f996",
+    "url": "/React-in-github-carousel/static/media/spiritedaway.e3a56a10.jpg"
+  },
+  {
+    "revision": "982903d060acb6bf1089c2cc3afd32a8",
+    "url": "/React-in-github-carousel/static/media/weatheringwithyou.982903d0.jpg"
+  },
+  {
+    "revision": "258d5837d18f5c3a44baa0f743c4ed83",
+    "url": "/React-in-github-carousel/static/media/yourname.258d5837.png"
+  }
+]);
